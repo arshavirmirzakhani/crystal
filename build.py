f53cf85398bbafb34e621fbdd90d1c747b05673f
arshavir mirzakhani <arshavir2468@gmail.com>
2025-01-08 23:27:36 +0330

    Update build.py

--- build.py
+++ build.py
@@ -1,11 +1,12 @@
 import subprocess
 import os
+import sys
 
 for p in ["crystal","crystal_bin","crystal_editor"]:
     
     print("\n-- Building: ",p," --\n")    
 
     os.chdir(p)
-    subprocess.run(["cargo","build"])
+    subprocess.run(["cargo","build"]+sys.argv[1:])
     os.chdir("..")
     
