093ca931e6bdff25af953b877680318259fea5fb
arshavir mirzakhani <arshavir2468@gmail.com>
2025-02-26 17:48:13 +0330

    updating wgpu experiments on crystal_bin

--- crystal_bin/src/main.rs
+++ crystal_bin/src/main.rs
@@ -3,71 +3,197 @@
 
 //
 // this project (crystal_bin) is the game executable itself. once game data is loaded it runs the required functions
-// and let the magic happen.
+// and let's the magic happen.
 //
 
 // for now im using it to test engine functionality
 
+use std::sync::Arc;
+use wgpu::MemoryHints::Performance;
 use winit::application::ApplicationHandler;
+use winit::error::EventLoopError;
 use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::event::{ElementState, KeyEvent};
+use winit::event_loop::ActiveEventLoop;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::keyboard::{Key, NamedKey};
 use winit::window::{Window, WindowId};
 
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
+pub struct WgpuCtx<'window> {
+    surface: wgpu::Surface<'window>,
+    surface_config: wgpu::SurfaceConfiguration,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
 }
 
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
-            event_loop
-                .create_window(Window::default_attributes())
-                .unwrap(),
-        );
+impl<'window> WgpuCtx<'window> {
+    pub async fn new_async(window: Arc<Window>) -> WgpuCtx<'window> {
+        let instance = wgpu::Instance::default();
+        let surface = instance.create_surface(Arc::clone(&window)).unwrap();
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                force_fallback_adapter: false,
+                // Request an adapter which can render to our surface
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .expect("Failed to find an appropriate adapter");
+        // Create the logical device and command queue
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
+                    required_limits: wgpu::Limits::downlevel_webgl2_defaults()
+                        .using_resolution(adapter.limits()),
+                    memory_hints: Performance,
+                },
+                None,
+            )
+            .await
+            .expect("Failed to create device");
+
+        let mut size = window.inner_size();
+        let width = size.width.max(1);
+        let height = size.height.max(1);
+        let surface_config = surface.get_default_config(&adapter, width, height).unwrap();
+        surface.configure(&device, &surface_config);
+
+        WgpuCtx {
+            surface,
+            surface_config,
+            adapter,
+            device,
+            queue,
+        }
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+    pub fn new(window: Arc<Window>) -> WgpuCtx<'window> {
+        pollster::block_on(WgpuCtx::new_async(window))
+    }
+
+    pub fn resize(&mut self, new_size: (u32, u32)) {
+        let (width, height) = new_size;
+        self.surface_config.width = width.max(1);
+        self.surface_config.height = height.max(1);
+        self.surface.configure(&self.device, &self.surface_config);
+    }
+
+    pub fn draw(&mut self) {
+        let surface_texture = self
+            .surface
+            .get_current_texture()
+            .expect("Failed to acquire next swap chain texture");
+        let texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        {
+            let _r_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &texture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+        }
+        self.queue.submit(Some(encoder.finish()));
+        surface_texture.present();
+    }
+}
+
+#[derive(Default)]
+pub struct App<'window> {
+    window: Option<Arc<Window>>,
+    wgpu_ctx: Option<WgpuCtx<'window>>,
+    flag: bool,
+}
+
+impl<'window> ApplicationHandler for App<'window> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.window.is_none() {
+            let win_attr = Window::default_attributes().with_title("wgpu winit example");
+            // use Arc.
+            let window = Arc::new(
+                event_loop
+                    .create_window(win_attr)
+                    .expect("create window err."),
+            );
+            self.window = Some(window.clone());
+            let wgpu_ctx = WgpuCtx::new(window.clone());
+            self.wgpu_ctx = Some(wgpu_ctx);
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
         match event {
             WindowEvent::CloseRequested => {
-                println!("The close button was pressed; stopping");
+
                 event_loop.exit();
             }
+            WindowEvent::Resized(new_size) => {
+                if let (Some(wgpu_ctx), Some(window)) =
+                    (self.wgpu_ctx.as_mut(), self.window.as_ref())
+                {
+                    wgpu_ctx.resize((new_size.width, new_size.height));
+                    window.request_redraw();
+                }
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        repeat: false,
+                        state: ElementState::Pressed,
+                        logical_key: Key::Named(NamedKey::Enter),
+                        ..
+                    },
+                ..
+            } => {
+                if let (Some(wgpu_ctx), Some(window)) =
+                    (self.wgpu_ctx.as_mut(), self.window.as_ref())
+                {
+                    let size = window.inner_size();
+                    let w = size.width.max(1);
+                    let h = size.height.max(1);
+                    if self.flag {
+                        wgpu_ctx.resize((w, h));
+                    } else {
+                        wgpu_ctx.resize((w / 2, h / 2));
+                    }
+                    self.flag = !self.flag;
+                    window.request_redraw();
+                }
+            }
             WindowEvent::RedrawRequested => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-
-                // Draw.
-
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
+                if let Some(wgpu_ctx) = self.wgpu_ctx.as_mut() {
+                    wgpu_ctx.draw();
+                }
             }
             _ => (),
         }
     }
 }
 
-fn main() {
+fn main() -> Result<(), EventLoopError> {
     let event_loop = EventLoop::new().unwrap();
-
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
     event_loop.set_control_flow(ControlFlow::Poll);
-
-    // ControlFlow::Wait pauses the event loop if no events are available to process.
-    // This is ideal for non-game applications that only update in response to user
-    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-    event_loop.set_control_flow(ControlFlow::Wait);
-
     let mut app = App::default();
-    let result = event_loop.run_app(&mut app);
-
-    assert!(result.is_err());
+    event_loop.run_app(&mut app)
 }
